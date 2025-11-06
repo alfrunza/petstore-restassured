34b79278be68837c76d020b245faba530ecdaa03
alfrunza <94307925+alfrunza@users.noreply.github.com>
2025-11-06 15:17:57 +0200

    Added Object Models for Pet and associated object types + getters and setters

--- src/main/java/models/Pet.java
+++ src/main/java/models/Pet.java
@@ -1,4 +1,55 @@
 package models;
 
+import java.util.List;
+
 public class Pet {
+    private long id;
+    private String name;
+    private Category category;
+    private String[] photoUrls;
+    private List<Tag> tags;
+    private Status status;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String[] getPhotoUrls() {
+        return photoUrls;
+    }
+    public void setPhotoUrls(String[] photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
 }
